77707a2e43ec15cdcce4f248ff4e009794a6ed5b
EsbonMigiro <esbonmigiro@gmail.com>
2024-12-04 00:17:36 +0300

    updated components

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,11 @@ import React, { useState, useRef, useEffect } from "react";
 function LandingPage() {
   const [showCamera, setShowCamera] = useState(false);
   const [capturedImage, setCapturedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
+  const [processedImage, setProcessedImage] = useState(null);
+  const serverApi = "http://138.68.191.35:5000";
+
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
@@ -53,9 +58,12 @@ function LandingPage() {
       formData.append("image", blob, filename);
 
       try {
-        const response = await fetch("http://10.96.0.20:5000/api/upload", {
+        const response = await fetch(`${serverApi}/api/upload`, {
           method: "POST",
           body: formData,
+          headers: {
+            Accept: "application/json",
+          },
         });
 
         const data = await response.json();
@@ -80,9 +88,60 @@ function LandingPage() {
       }
     };
   }, []);
+  //-----------------------------
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file)); // Display the selected image
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!selectedImage) {
+      setUploadStatus("Please select an image first.");
+      return;
+    }
+
+    const formData = new FormData();
+    const file = document.getElementById("image-input").files[0];
+
+    // Generate a unique filename based on the current datetime
+    const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
+    const filename = `uploaded_image_${timestamp}.png`;
+
+    formData.append("image", file, filename);
+
+    try {
+      setUploadStatus("Uploading...");
+      const response = await fetch(`${serverApi}/api/upload`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setUploadStatus("Image uploaded successfully!");
+        console.log("Image uploaded successfully:", data);
+        setProcessedImage(`${serverApi}/downloads/${data.filename}`);
+      } else {
+        setUploadStatus("Error uploading image.");
+        console.error("Error uploading image:", data);
+      }
+    } catch (error) {
+      setUploadStatus("Error sending image to server.");
+      console.error("Error sending image to server:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(processedImage);
+  }, [processedImage]);
   return (
-    <div className="h-screen flex w-full justify-center bg-gray-500 my-8">
+    <div className=" flex w-full flex-col items-center justify-center bg-gray-500 my-8">
       {/* Container Div */}
       <div className="h-screen flex flex-col lg:w-1/2">
         {/* Top Div */}
@@ -122,6 +181,15 @@ function LandingPage() {
               Process
             </button>
           )}
+
+          {showCamera && (
+            <button
+              onClick={handleUpload}
+              className="absolute bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 focus:outline-none"
+            >
+              upload
+            </button>
+          )}
         </div>
 
         {/* Bottom Div */}
@@ -141,6 +209,38 @@ function LandingPage() {
             />
           )}
         </div>
+      </div>
+
+      <div className="flex justify-center items-center flex-col lg:w-1/2">
+        <input
+          type="file"
+          id="image-input"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="mb-4 lg:w-1/2"
+        />
+        {selectedImage && (
+          <img
+            src={selectedImage}
+            alt="Selected"
+            className="max-w-full h-auto mb-4"
+          />
+        )}
+        <button
+          onClick={handleUpload}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Upload Image
+        </button>
+        {uploadStatus && <p className="mt-4 text-white">{uploadStatus}</p>}
+
+        {processedImage && (
+          <img
+            src={processedImage}
+            alt="Selected"
+            className="max-w-full h-auto mb-4"
+          />
+        )}
       </div>
     </div>
   );
